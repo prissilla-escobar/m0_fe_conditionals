e0c5106a79a02c7548bf0a10038419206dcd6f7b
Prissilla Escobar <prissilla988@gmail.com>
2023-04-19 12:56:28 -0600

    Initial Commit

--- ifStatements.js
+++ ifStatements.js
@@ -42,10 +42,13 @@ if (weather === "sunny") {
 // Experiment with manipulating the value held within numQuarters
 // to make sure both conditions can be achieved.
 
-var numQuarters = 0
+var numQuarters = 19
 
+if (numQuarters >= 2) {
 console.log("I have enough money for a gumball");
+} else if (numQuarters < 2) {
 console.log("I don't have enough money for a gumball");
+}
 
 
 // #####################
